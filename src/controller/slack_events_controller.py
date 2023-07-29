d82f680ee6857e0fd7c1323ec76d95e533901ec0
gushono <gustavo.honorato@mercadolivre.com>
2023-07-29 14:48:57 -0300

    adding links as attachment

--- src/controller/slack_events_controller.py
+++ src/controller/slack_events_controller.py
@@ -112,8 +112,7 @@ def echo_interactive():  # pragma: no cover
             slack_client.client.chat_postMessage(
                 thread_ts=payload["message"]["thread_ts"],
                 channel=payload["channel"]["id"],
-                # blocks=blocks_links_secure_code_warriors['blocks'],
-                attachments=blocks_links_secure_code_warriors['attachments']
+                blocks=blocks_links_secure_code_warriors['blocks'],
             )
 
         if payload['actions'][0]['value'] == 'click_me_123':
