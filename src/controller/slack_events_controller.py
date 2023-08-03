4712fca916315dd27dfc5a722d3f9255fec43549
gushono <gustavo.honorato@mercadolivre.com>
2023-08-03 10:52:43 -0300

    adding event endpoint

--- src/controller/slack_events_controller.py
+++ src/controller/slack_events_controller.py
@@ -3,8 +3,11 @@ import json
 from flask import Blueprint, request, Response, jsonify
 from slack_sdk.signature import SignatureVerifier
 
+from src.client.slack_client import SlackClient
 from src.environment import env
+from src.services.interactive_service.actions_slack_blocks import get_block_initial_message
 from src.services.interactive_service.interactive_service import handle_actions, handle_view_flow
+from src.services.interactive_service.views_strategy import HomeStrategy
 from src.services.slack_service import SlackService
 
 slack_events_blueprint = Blueprint('slack_events', __name__)
@@ -20,17 +23,29 @@ def handle_slack_events():
     if not signature_verifier.is_valid_request(request.get_data(), request.headers):
         return jsonify({"error": "Invalid request"}), 403
 
+    events_handlers = {
+        "message": handle_message,
+        "app_mention": handle_mentions,
+        "app_home_opened": handle_app_home_opened
+
+    }
+
     data = request.get_json()
     print("this is the value")
     print(data)
-    print("this is the data")
+    print("this is the data type")
     print(data)
 
     slack_service = SlackService()
 
     bot_id = slack_service._slack_client.client.api_call("auth.test")["user_id"]
-
+    event_type = data.get("type")
     if "event" in data and "text" in data["event"]:
+        handler = events_handlers.get(event_type)
+
+        if not handler:
+            raise ValueError(f"Handler for event {event_type} not implemented.")
+
         user = data["event"].get("user")
 
         if not user or user == bot_id:
@@ -41,6 +56,9 @@ def handle_slack_events():
             channel="C04GL827WKX",
             text=f"This is the event: {json.dumps(event)}",
         )
+
+        result = handler(data)
+        print(result)
 
         return Response(), 200
 
@@ -54,56 +72,52 @@ def handle_slack_events():
 
 # Create a SlackClient for your bot to use for Web API requests
 
-#
-# # Example responder to bot mentions
+
+# Example responder to bot mentions
 # @slack_events_adapter.on("app_mention")  # pragma: no cover
-# def handle_mentions(event_data):
-#     print("this is the event")
-#     print(event_data)
-#     # slack_client = SlackClient()
-#     # event = event_data["event"]
-#     # slack_client.send_message(
-#     #     channel=event["channel"],
-#     #     message=f"You said:\n>{event['text']}",
-#     # )
-#
-#
-# # Example responder to greetings
+def handle_mentions(event_data):
+    print("this is the event")
+    print(event_data)
+    # slack_client = SlackClient()
+    # event = event_data["event"]
+    # slack_client.send_message(
+    #     channel=event["channel"],
+    #     message=f"You said:\n>{event['text']}",
+    # )
+
+
+# Example responder to greetings
 # @slack_events_adapter.on("message")
-# def handle_message(event_data):
-#     slack_client = SlackClient()
-#
-#     event = event_data["event"]
-#     user = event.get("user")
-#
-#     print(event)
-#
-#     bot_id = slack_client.client.api_call("auth.test")["user_id"]
-#
-#     if not user or user == bot_id:
-#         return
-#
-#     allowed_channels = ["C04GL827WKX"]
-#
-#     if event.get("channel") is not None and event.get("channel") not in allowed_channels:
-#         print(event.get("channel"))
-#         print("Not allowed channel")
-#         return Response(), 200
-#     print("this is the event")
-#     print(event)
-#
-#     # if event['text'].lower() == "start":
-#     #     send_welcome_message(f'@{user}', user=user, slack_client=slack_client.client, payload=event)
-#     #     return
-#     # else:
-#     initial_message = get_block_initial_message()
-#     ts_thread = event.get("ts")
-#     slack_client.client.chat_postMessage(
-#         thread_ts=ts_thread,
-#         channel=event["channel"],
-#         text=event['text'],
-#         blocks=initial_message['blocks']
-#     )
+def handle_message(event_data):
+    slack_client = SlackClient()
+
+    event = event_data["event"]
+    user = event.get("user")
+
+    print(event)
+
+    bot_id = slack_client.client.api_call("auth.test")["user_id"]
+
+    if not user or user == bot_id:
+        return
+
+    allowed_channels = ["C04GL827WKX"]
+
+    if event.get("channel") is not None and event.get("channel") not in allowed_channels:
+        print(event.get("channel"))
+        print("Not allowed channel")
+        return Response(), 200
+    print("this is the event")
+    print(event)
+
+    initial_message = get_block_initial_message()
+    ts_thread = event.get("ts")
+    slack_client.client.chat_postMessage(
+        thread_ts=ts_thread,
+        channel=event["channel"],
+        text=event['text'],
+        blocks=initial_message['blocks']
+    )
 
 
 @slack_events_blueprint.route("/interactive-endpoint", methods=["POST"])
@@ -127,25 +141,19 @@ def echo_interactive():  # pragma: no cover
     print(payload)
     return {"opa": "opa"}, 200
 
+
 #
 # @slack_events_adapter.on("app_home_opened")
-# def app_home_opened(event_data):
-#     print(event_data)
-#     message = event_data["event"]
-#
-#     response = HomeStrategy().execute(payload=message, slack_service=SlackService())
-#
-#     return Response(response), 200
-#
-#
-# # Error events
+def handle_app_home_opened(event_data):
+    print(event_data)
+    message = event_data["event"]
+
+    response = HomeStrategy().execute(payload=message, slack_service=SlackService())
+
+    return Response(response), 200
+
+
+# Error events
 # @slack_events_adapter.on("error")
-# def error_handler(err):  # pragma: no cover
-#     print("ERROR: " + str(err))
-#
-#
-# def format_values_slack(values: dict):  # pragma: no cover
-#     subject = values['input_select']['static_select-action']['selected_option']['value'].lower()
-#     message = values['block_id']['input_question']['value']
-#
-#     return subject, message
+def error_handler(err):  # pragma: no cover
+    print("ERROR: " + str(err))
