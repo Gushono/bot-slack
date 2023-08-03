b9c2c90124a337f6e1002951037a14ee6d59b6a8
gushono <gustavo.honorato@mercadolivre.com>
2023-08-03 18:12:28 -0300

    send slack message as test

--- src/services/events_service/events_service.py
+++ src/services/events_service/events_service.py
@@ -1,3 +1,5 @@
+import json
+
 from flask import Response
 
 from src.services.events_service.events_strategy import (
@@ -18,6 +20,11 @@ def handle_events(event: dict) -> tuple[Response, int]:
     slack_service = SlackService()
     if slack_service.is_message_from_bot(event):
         return Response(), 200
+
+    slack_service.send_slack_message(
+        channel="C05KYUZ3LF2",
+        text=f"Esse é o evento {json.dumps(event)}",
+    )
 
     events_strategy_handler = EVENTS_HANDLERS_STRATEGY.get(event["type"])
     if not events_strategy_handler:
